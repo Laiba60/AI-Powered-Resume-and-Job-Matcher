d86ee408b972d3c8bef380a629ec6e5670deca29
Laiba60 <laibasaeed026@gmail.com>
2026-03-09 15:13:52 +0500

    Add reusme code

--- frontend/src/components/ResumeUploadTopbar.jsx
+++ frontend/src/components/ResumeUploadTopbar.jsx
@@ -1,6 +1,6 @@
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { FaUserCircle } from "react-icons/fa";
-export default function Topbar({ title }) {
+export default function ResumeUploadTopbar({ title }) {
   return (
     <div className="bg-white  px-6 py-4 flex justify-between items-center">
       <h2 className="text-lg font-semibold">{title}</h2>
--- frontend/src/pages/UploadResume.jsx
+++ frontend/src/pages/UploadResume.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "../components/ResumeSidebar";
-import Topbar from "../components/ResumeUploadTopbar";
+import ResumeUploadTopbar from "../components/ResumeUploadTopbar";
 import UploadCard from "../components/UploadCard";
 
 export default function ResumeUpload() {
@@ -8,7 +8,7 @@ export default function ResumeUpload() {
       <Sidebar />
 
       <div className="flex-1 flex flex-col">
-        <Topbar title="Resume Upload" />
+        <ResumeUploadTopbar title="Resume Upload" />
 
         <div className="flex justify-center items-center flex-1 p-6">
           <UploadCard />
