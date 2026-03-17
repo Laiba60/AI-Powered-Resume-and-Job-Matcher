b18dcb84562dee8cc3b2cbe4c81b05e8790d08d7
Laiba60 <laibasaeed026@gmail.com>
2026-03-17 16:14:24 +0500

    initial commit

--- frontend/src/components/Admin/SkillBadge.jsx
+++
@@ -1,7 +0,0 @@
-export default function SkillBadge({ skill }) {
-  return (
-    <span className="bg-indigo-600/20 text-indigo-400 px-2 py-1 rounded-md text-xs">
-      {skill}
-    </span>
-  );
-}
