6878de5593a5ea340573d21f458d663e168c7bd4
Laiba60 <laibasaeed026@gmail.com>
2026-06-07 20:58:52 +0500

    improve code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import UserDashboard from "./pages/UserDashboard";
+
 
 function App() {
   return (
@@ -10,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/userdashboard" element={<UserDashboard />} />
+       
+      
        
       </Routes>
     </BrowserRouter>
